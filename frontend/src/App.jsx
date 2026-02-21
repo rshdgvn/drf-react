abda1a02614f583511979623899eeb18ca543620
rshdgvn <rasheedgavinesponga@gmail.com>
2026-02-22 00:01:49 +0800

    done with the frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,40 @@
-import { View, Text } from 'react-native'
-import React from 'react'
 
-const App = () => {
+import react from "react"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import Home from "./pages/Home"
+import NotFound from "./pages/NotFound"
+import ProtectedRoute from "./components/ProtectedRoute"
+
+function Logout() {
+  localStorage.clear()
+  return <Navigate to="/login" />
+}
+
+function RegisterAndLogout() {
+  localStorage.clear()
+  return <Register />
+}
+
+function App() {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/register" element={<RegisterAndLogout />} />
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
