033fa3b1e370b321f23885e1b595a2a4e55b7a4a
rshdgvn <rasheedgavinesponga@gmail.com>
2026-02-22 00:06:41 +0800

    fix urls

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('notes/', views.NoteDelete.as_view(), name="note-lisy"),
+  path('notes/', views.NoteListCreate.as_view(), name="note-lisy"),
   path('notes/delete/<int:pk>/', views.NoteDelete.as_view(), name="delete-note")
 ] 
